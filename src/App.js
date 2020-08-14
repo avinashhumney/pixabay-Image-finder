3d09f2471680f70b01f51cb9a6ce91c0b62fbe96
Avinash Humney <avinash.humney@outlook.com>
2020-08-15 01:10:06 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Navbar from './Components/Navbar';
+import Search from './Components/Search'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <Navbar />
+          <Search/>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar'
+
+
+function Navbar() {
+    return(
+        <AppBar title="Pixabay Image Finder"/>
+    )
+}
+
+export default Navbar
